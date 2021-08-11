dbb834e74f7a8a04c254b695fe62c05783f78718
David Gleich <gleichd@mediamarktsaturn.com>
2021-08-11 17:32:42 +0200

    Initial version with a loop

--- molecule/default/tests/test_executables.py
+++ molecule/default/tests/test_executables.py
@@ -1,33 +1,19 @@
 import os
 import stat
 
-def test_exist_bottom(host):
-    home = host.user("root").home
-    assert host.file(home+"/.local/bin/btm").exists
-    assert host.file(home+"/.local/bin/btm").mode == 0o755
+binaries = [
+    "btm",
+    "bat",
+    "dyff",
+    "helm",
+    "istioctl",
+    "kind",
+    "yq"
+]
 
-def test_exist_zoxide(host):
+def test_binaries_exist(host):
     home = host.user("root").home
-    assert host.file(home+"/.local/bin/dua").exists
-    assert host.file(home+"/.local/bin/dua").mode == 0o755
-
-def test_exist_zoxide(host):
-    home = host.user("root").home
-    assert host.file(home+"/.local/bin/zoxide").exists
-    assert host.file(home+"/.local/bin/zoxide").mode == 0o755
-
-def test_exist_tokei(host):
-    home = host.user("root").home
-    assert host.file(home+"/.local/bin/tokei").exists
-    assert host.file(home+"/.local/bin/tokei").mode == 0o755
-
-def test_exist_dust(host):
-    home = host.user("root").home
-    assert host.file(home+"/.local/bin/dust").exists
-    assert host.file(home+"/.local/bin/dust").mode == 0o755
-
-def test_exist_dyff(host):
-    home = host.user("root").home
-    assert host.file(home+"/.local/bin/dyff").exists
-    assert host.file(home+"/.local/bin/dyff").mode == 0o755
-
+    for bin in binaries:
+        filepath = os.path.join(home, ".local/bin", bin)
+        assert host.file(filepath).exists
+        assert host.file(filepath).mode == 0o755
